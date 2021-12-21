4c2d35297f978c03bea9af293a58cc248090f5f0
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2021-12-20 23:22:13 -0500

    se agregó el registro

--- scripts/register.js
+++ scripts/register.js
@@ -1,22 +1,28 @@
 let formulario = document.getElementById('formulario')
-let guardar = document.getElementById('btnSubmit')
+let guardar = document.getElementById('btnBuscar')
 
-let register =[]
+let register =JSON.parse(localStorage.getItem('Register')) || []
+
 const capturaDatos = ()=>{
     let nombre = document.getElementById('nombre').value
-    let numero = document.getElementById('numero').value
     let password = document.getElementById('password').value
     
     let registro ={
         nombre,
-        numero,
         password
     }
 
-    // register.unshift(registro)
-    // localStorage.setItem('Register', JSON.stringify(register))
-    // getLocalStorage();
+     register.unshift(registro)
+     localStorage.setItem('Register', JSON.stringify(register))
+     getLocalStorage();
     console.log(register)
+}
+
+const getLocalStorage = () =>{
+
+    let registrarLocalStorage = JSON.parse(localStorage.getItem('Register'))
+    console.log(registrarLocalStorage);
+  
 }
 
 formulario.addEventListener('submit', e =>{
@@ -24,3 +30,5 @@ formulario.addEventListener('submit', e =>{
     capturaDatos()
    
 })
+document.addEventListener('DOMContentLoaded', getLocalStorage)
+
